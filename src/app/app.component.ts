35e48cfdf854dcdffad82f59c8f4ef0cdc3cae50
aljary12 <37587027+aljary12@users.noreply.github.com>
2018-04-09 15:53:57 +0800

    Log Out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { LoginPage } from '../pages/login/login';
+import { AngularFireAuth } from "angularfire2/auth";
 
 @Component({
   templateUrl: 'app.html'
@@ -17,14 +18,19 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    private afAuth : AngularFireAuth) {
+
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: "Login", component: LoginPage}
+      { title: 'Log Out', component: LoginPage}
     ];
 
   }
@@ -41,6 +47,10 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    if (page.component == LoginPage) {
+      this.afAuth.auth.signOut().then(() => this.nav.setRoot(page.component));
+    } else {
+      this.nav.setRoot(page.component);
+    }
   }
 }
