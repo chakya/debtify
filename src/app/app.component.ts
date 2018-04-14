cb5dfb1db2ded3d8ea9b7b2d7c31c02afb807e61
chakya <chkya8@gmail.com>
2018-04-14 21:28:26 +0800

    first merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,69 +6,114 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { DebtCalcPage } from '../pages/debt-calc/debt-calc';
-import firebase from 'firebase';
 import { LoginPage } from '../pages/login/login';
+
+
+
 @Component({
+
   templateUrl: 'app.html'
+
 })
 
 
 
+
+
+
+
 export class MyApp {
+
   @ViewChild(Nav) nav: Nav;
+
+
 
   rootPage: any;
 
+
+
   pages: Array<{title: string, component: any}>;
 
+
+
   constructor(
+
     public platform: Platform, 
+
     public statusBar: StatusBar,
+
     public splashScreen: SplashScreen,
+
     private auth: AuthProvider) {
+
     
+
     const authObserver = auth.currentUserObservable().subscribe(user => {
+
       this.rootPage = user && user.emailVerified ? HomePage : LoginPage;
+
       authObserver.unsubscribe();
+
     });
+
+
 
     this.initializeApp();
 
+
+
     // used for an example of ngFor and navigation
+
     this.pages = [
+
       { title: 'Home', component: HomePage },
+
       { title: 'List', component: ListPage },
+
       { title: 'Log Out', component: LoginPage}
+
     ];
 
-    firebase.initializeApp({
-      apiKey: 'AIzaSyA--qMbmrqws4llh_EbW3AJNue_R0__I9A',
-      authDomain: 'debtify-00.firebaseapp.com',
-      databaseURL: 'https://debtify-00.firebaseio.com',
-      storageBucket: 'debtify-00.appspot.com',
-      messagingSenderId: '942254922740',
-    });
+
 
   }
+
+
 
   initializeApp() {
+
     this.platform.ready().then(() => {
+
       // Okay, so the platform is ready and our plugins are available.
+
       // Here you can do any higher level native things you might need.
+
       this.statusBar.styleDefault();
+
       this.splashScreen.hide();
+
     });
+
   }
 
+
+
   openPage(page) {
+
     // Reset the content nav to have just this page
+
     // we wouldn't want the back button to show in this scenario
+
     if (page.component == LoginPage) {
+
       this.auth.logoutUser().then(() => this.nav.setRoot(page.component));
+
     } else {
+
       this.nav.setRoot(page.component);
+
     }
+
   }
 
 }
