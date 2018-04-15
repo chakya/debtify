d4e3118b927c9c0d8a29b10a5df13f2ee5a08c15
aljary12 <37587027+aljary12@users.noreply.github.com>
2018-04-15 16:04:54 +0800

    contact Provider

--- src/providers/contact/contact.ts
+++ src/providers/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs/Observable';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 
@@ -12,14 +12,14 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ContactProvider {
 
-  private contact: Observable<any>;
+  private contact: AngularFireList<any>;
 
   constructor(public auth: AngularFireAuth, public db: AngularFireDatabase) {
-    this.contact = this.db.list("Contact/0").valueChanges();
+    this.contact = this.db.list("Contact/0");
   }
 
   getContact() {
-    return this.contact;
+    return this.contact.valueChanges();
   }
 
 }
