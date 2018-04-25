d420f52270e2e6215e8f256d7b37752942a7d85c
chakya <chkya8@gmail.com>
2018-04-25 19:55:40 +0800

    Ryadi checkusername

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -4,6 +4,7 @@ import { IonicPage, NavController, NavParams} from 'ionic-angular';
 import { User } from '../../models/user';
 import { AuthProvider } from './../../providers/auth/auth';
 import { LoginPage } from '../login/login';
+import { DebtifyDatabaseProvider } from '../../providers/debtify-database/debtify-database';
 
 /**
  * Generated class for the RegisterPage page.
@@ -18,24 +19,42 @@ import { LoginPage } from '../login/login';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
-  user = {} as User;
+  
+  fullname: string;
+  username: string;
+  email: string;
+  password: string;
+  usernameAvailable: boolean;
 
   constructor(
     public navCtrl: NavController, public navParams: NavParams,
-    public utils: UtilsProvider, public authData: AuthProvider) {
+    public utils: UtilsProvider, public authData: AuthProvider,
+    public debtifyDb: DebtifyDatabaseProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
 
+  checkUsername() {
+    if (this.username.length < 6) {
+      this.usernameAvailable = false;
+      return;
+    }
+    this.debtifyDb.checkUsername(this.username).subscribe(username => {
+      this.usernameAvailable = !username;
+    })
+  }
 
   register() {
     this.utils.createLoading();
-    this.authData.signupUser(this.user.email, this.user.password)
+    this.authData.signupUser(this.email, this.password)
       .then(user => {
         user.sendEmailVerification().then(() => {
-          this.utils.dismissLoading().then(() => this.utils.createAlert("Please verify your email", "OK", () => this.navCtrl.setRoot(LoginPage)));
+          this.utils.dismissLoading().then(() => this.utils.createAlert("Please verify your email", "OK", () => {
+            this.navCtrl.setRoot(LoginPage);
+            this.debtifyDb.registerUser(this.username, this.fullname, user.uid);
+          }));
         }, (error) => {
           this.utils.dismissLoading().then(() => this.utils.createAlert(error.message, "OK"));
         });
